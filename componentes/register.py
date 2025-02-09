d7bebbc9e7375c3be8560ed50c3eda4b29cc6f59
Angel Mares <chuisangel8@gmail.com>
2025-02-09 16:53:09 -0600

    add drop menu for user type

--- componentes/register.py
+++ componentes/register.py
@@ -1,5 +1,7 @@
+from kivy.uix.accordion import BooleanProperty
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.textfield import MDTextField
+from kivymd.uix.menu import MDDropdownMenu
 
 from bcrypt import gensalt,hashpw
 from pymongo import MongoClient
@@ -11,14 +13,18 @@ from kivymd.uix.dialog import (
     MDDialogHeadlineText
 )
 
+from kivy.properties import BooleanProperty
+
 class RegisterScreen(Screen):
+    user_type_validator = BooleanProperty(True)
+
     def register(self):
         doc = {
             'name': self.ids.input_nombre.text, 
             'last_name': self.ids.input_apellido.text, 
             'email': self.ids.input_email.text, 
             'password': hashpw(self.ids.input_password.text.encode('utf-8'), gensalt()), 
-            'type': 'encuestador'
+            'type': self.ids.input_user_type.text
             }
         try:
             with MongoClient(getenv('MONGO_URI')) as mongo:
@@ -45,6 +51,18 @@ class RegisterScreen(Screen):
                         text='No se pudo crear el usuario.'
                     )
                 ).open()
+    def open_menu(self, item):
+        menu_items = [
+            {
+                "text": f"{data}",
+                "on_release": lambda x=f"{data}": self.menu_callback(x),
+            } for data in ['Encuestador', 'Investigador']
+        ]
+        self.user_type_validator = False
+        MDDropdownMenu(caller=item, items=menu_items).open()
+
+    def menu_callback(self, text_item):
+        self.ids.input_user_type.text = text_item
 
 class CompareField(MDTextField):
     def compare(self, other: MDTextField):
