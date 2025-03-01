3ba547835bea1855693374647dea6e7ab11334c0
Angel Mares <chuisangel8@gmail.com>
2025-03-01 17:17:24 -0600

    add Encuestas Screen reading files from directory "encuestas/"

--- componentes/encuestas.py
+++ componentes/encuestas.py
@@ -1,21 +1,26 @@
 from kivymd.uix.card import MDCard
 from kivy.properties import StringProperty
 from kivy.uix.screenmanager import Screen
-from form.retrieve import get_forms
 from os import listdir
-from logging import debug
+from json import load
 
 class EListItem(MDCard):
     title = StringProperty()
     formId = StringProperty()
+    description = StringProperty()
 
-    def save():
+    def save(self):
         pass
 
 class EncuestasScreen(Screen):
-    def on_enter(self, *args):
-        self.ids.rv.data = [{
-            'title': 'Encuesta',
-            'formId': file
-        }for file in listdir('encuestas') ]
-        debug(f"{self.ids.rv.height} {self.ids.rv.parent.height}")
+    def on_enter(self):
+        data = list()
+        for form in listdir('encuestas'):
+            with open('encuestas/'+form,'r') as form_content:
+                form_dict = load(form_content)
+                data.append({
+                    'formId':form_dict['formId'],
+                    'title':form_dict['sections'][0]['title'],
+                    'description':form_dict['sections'][0]['description'].strip()
+                })
+        self.ids.rv.data = data
